5cc67ac4dfcb99a6a90dbd440e7ac891c5b0a532
skillz <skillzdev@hotmail.com>
2023-04-29 21:03:47 +0100

    added tp5 solution

--- TP5/app/src/main/java/com/example/tp5/MainActivity.java
+++ TP5/app/src/main/java/com/example/tp5/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tp5;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +11,6 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,61 +19,90 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
     Messenger mService = null;
+
+
     boolean isConnected = false;
-    TextView tvVal;
-    String stringInitialTVal ;
-    private class ClientHandler extends Handler {
-        @Override
-        public void handleMessage( Message msg) {
-            int valCouranteChrono = msg.arg1;
-            Log.i("COMPTEUR COURANT ", "VALEUR ACTUELLE DU CHRONO = "+ valCouranteChrono);
-            tvVal.setText(stringInitialTVal + valCouranteChrono);
-        } }
-    private ServiceConnection mConnection = new ServiceConnection() {
+    private final ServiceConnection mConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName className, IBinder service) {
-            mService = null;
-            isConnected = true;}
+            //TODO MY SOLUTION
+            mService = new Messenger(service);
+            //TODO END OF SOLUTION
+            isConnected = true;
+        }
+
         public void onServiceDisconnected(ComponentName className) {
             mService = null;
             isConnected = false;
-        }};
+        }
+    };
+    TextView tvVal;
+    String stringInitialTVal;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button boutAtt = (Button) findViewById(R.id.btn);
-        Button boutDett = (Button) findViewById(R.id.btn1);
-        Button boutVal = (Button) findViewById(R.id.btn2);
-        TextView tvEtat = (TextView) findViewById(R.id.header);
-        tvVal = (TextView) findViewById(R.id.chorus);
+        Button boutAtt = (Button) findViewById(R.id.button);
+        Button boutDett = (Button) findViewById(R.id.button2);
+        Button boutVal = (Button) findViewById(R.id.button3);
+        TextView tvEtat = (TextView) findViewById(R.id.textView);
+        tvVal = (TextView) findViewById(R.id.textView2);
         String stringInitialTvEta = (String) tvEtat.getText();
         stringInitialTVal = (String) tvVal.getText();
-        boutAtt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        boutAtt.setOnClickListener(v -> {
+            try {
                 Intent monIntent = new Intent();
-                monIntent.setComponent(new ComponentName("???","???"));
+                //TODO MY SOLUTION
+                monIntent.setComponent(new ComponentName("com.example.tp5",
+                        "com.example.tp5.MonService"));
+                //TODO END OF MY SOLUTION
                 bindService(monIntent, mConnection,
                         Context.BIND_AUTO_CREATE);
-                Toast.makeText(MainActivity.this," Liaison établie ", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, " Liaison établie ", Toast.LENGTH_LONG).show();
                 tvEtat.setText(stringInitialTvEta + " ATTACHEE ");
                 isConnected = true;
-            } });
-        boutDett.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isConnected) {
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        boutDett.setOnClickListener(v -> {
+            try {
+                if (isConnected) {
                     unbindService(mConnection);
                     Toast.makeText(MainActivity.this, " Liaison coupée ", Toast.LENGTH_LONG).show();
                     tvEtat.setText(stringInitialTvEta + " DETACHEE ");
-                    isConnected = false; }}});
-        boutVal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                    isConnected = false;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        });
+        boutVal.setOnClickListener(v -> {
+            //TODO MY SOLUTION
+            if (isConnected) {
+                Message msg = Message.obtain();
+                msg.arg1 = 1;
+                msg.replyTo = new Messenger(new ClientHandler());
+                try {
+                    mService.send(msg);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            //TODO END OF SOLUTION
+        });
+    }
+
+    @SuppressLint("HandlerLeak")
+    private class ClientHandler extends Handler {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void handleMessage(Message msg) {
+            int valCouranteChrono = msg.arg1;
+            Log.i("COMPTEUR COURANT ", "VALEUR ACTUELLE DU CHRONO = " + valCouranteChrono);
+            tvVal.setText(stringInitialTVal + valCouranteChrono);
         }
-        );
     }
 }
--- TP5/app/src/main/java/com/example/tp5/MonService.java
+++ TP5/app/src/main/java/com/example/tp5/MonService.java
@@ -1,5 +1,6 @@
 package com.example.tp5;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
@@ -10,43 +11,59 @@ import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
 
+
 public class MonService extends Service {
-    public MonService() {
-    }
+    private final Handler handler = new Handler();
+    //TODO MY SOLUTION
+    Messenger mMessenger = new Messenger(new HandlerEntrant());
     private int count = 0;
-    private Handler handler= new Handler();
-    Messenger mMessenger = null ;
-    class HandlerEntrant extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            if(msg.arg1==1){
-                Message reponseMsgVal = Message.obtain();
-                reponseMsgVal.arg1= count;
-                try {
-                    msg.replyTo.send(reponseMsgVal); // envoyer la réponse à l’émetteur (client)
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }}}}
-    // est appelee lorsqu'on execute bindService
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.i("Start ", "Service Chrono BIND " );
-        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
-        handler.postDelayed(runnable, 0);
-        return null;
-    }
-    // est appelee lorsqu'on execute unbindService
-    @Override
-    public void onDestroy() {
-        Log.i("Destroy", "Service Chrono Arrêté ");
-        Toast.makeText(getApplicationContext(),"OnDestroy: Service Chrono Arrêté avec la valeur " + count ,
-                Toast.LENGTH_LONG).show();
-        handler.removeCallbacks(runnable); // retirer la tâche runnable de la file d'attente du handler
-    }
     // Tâche d'arrière plan qui incrémente le compteur de 1 à chaque seconde.
-    private Runnable runnable = new Runnable() {
+    private final Runnable runnable = new Runnable() {
         @Override
         public void run() {
             count++;
             Log.i("Exec Service Chrono", "Valeur du compteur = " + count);
-            handler.postDelayed(this, 1000); } }; }
+            handler.postDelayed(this, 1000);
+        }
+    };
+
+    public MonService() {
+    }
+
+    // est appelee lorsqu'on execute bindService
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("Start ", "Service Chrono BIND ");
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        handler.postDelayed(runnable, 0);
+        //TODO MY SOLUTION
+        return mMessenger.getBinder();
+        //TODO END OF SOLUTION
+    }
+
+    // est appelee lorsqu'on execute unbindService
+    @Override
+    public void onDestroy() {
+        Log.i("Destroy", "Service Chrono Arrêté ");
+        Toast.makeText(getApplicationContext(), "OnDestroy: Service Chrono Arrêté avec la valeur " + count,
+                Toast.LENGTH_LONG).show();
+        handler.removeCallbacks(runnable); // retirer la tâche runnable de la file d'attente du handler
+    }
+
+    //TODO END OF SOLUTION
+    @SuppressLint("HandlerLeak")
+    class HandlerEntrant extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.arg1 == 1) {
+                Message reponseMsgVal = Message.obtain();
+                reponseMsgVal.arg1 = count;
+                try {
+                    msg.replyTo.send(reponseMsgVal); // envoyer la réponse à l’émetteur (client)
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
